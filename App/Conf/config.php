c34688982520a93b8af25d23522a91ecf7f2d34b
songwc <songwc888@gmail.com>
2014-04-25 21:06:27 +0800

    songwc

---
+++ App/Conf/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'APP_GROUP_LIST'=>'Index,Admin',
+	'DEFAULT_GROUP'=>'Index',
+	//模板文件的后缀名
+	'TMPL_TEMPLATE_SUFFIX'=>'.html',
+		//U函数生成路径的后缀名
+		'URL_HTML_SUFFIX'=>'html',
+		//使用PATHINFO路径
+		//'URL_MODEL'=>1,
+		
+);
+?>
---
+++ App/Lib/Action/Index/IndexAction.class.php
@@ -0,0 +1,7 @@
+<?php
+Class IndexAction extends Action{
+	Public function Index(){
+		$this->display();
+	}
+}
+?>
